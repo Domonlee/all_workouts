e06aec6999b11742df56d896ec67276e2a148e54
Dennis Li <vdplizhao@gmail.com>
2026-09-04 18:49:23 +0800

    更新站点元数据以反映新的标题、描述和导航链接

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,16 +16,17 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Dennis Running Page',
-  siteUrl: 'https://run.domon.cn',
-  logo: 'https://domon.cn/content/images/2020/06/avatar_shiba-2.jpg',
-  description: '在路上，跑起来',
+  siteTitle: "Dennis Running Page",
+  siteUrl: "https://run.domon.cn",
+  logo: "https://domon.cn/content/images/2020/06/avatar_shiba-2.jpg",
+  keywords: "workouts, running, cycling, riding, roadtrip, hiking, swimming",
+  description: "在路上，跑起来",
   navLinks: [
-    { name: 'Blog', url: 'https://www.domon.cn' },
-    { name: 'Moment',url: 'https://t.me/s/hualihuawai' },
-    { name: 'Memos', url: 'https://memos.domon.cn' },
-    { name: 'Link-Exchange',url: 'https://domon.cn/link-exchange/' },
-    { name: 'About',url: 'https://www.domon.cn/about' },
+    { name: "Blog", url: "https://www.domon.cn" },
+    { name: "Moment", url: "https://t.me/s/hualihuawai" },
+    { name: "Memos", url: "https://memos.domon.cn" },
+    { name: "Link-Exchange", url: "https://domon.cn/link-exchange/" },
+    { name: "About", url: "https://www.domon.cn/about" },
   ],
 };
 
